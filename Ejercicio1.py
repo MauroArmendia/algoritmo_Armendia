097a3dcb19163b77da3c90df9a273b9f00adb9bf
MauroArmendia <mauro.armendiaet32@gmail.com>
2025-12-05 11:10:28 -0300

    Refactor Ejercicio1.py and add new exercises
    Signed-off-by: MauroArmendia <mauro.armendiaet32@gmail.com>

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -26,7 +26,7 @@ def ejercio1 ():
       if menu == 3:
          print("Cerrando el programa")
          break
-ejercio1()
+
 
 def ejercicio2 ():
    try:
@@ -43,4 +43,70 @@ def ejercicio2 ():
          print("Usted tiene obesidad")
    except ValueError:
       print("Ingrese un valor apropiado")
-ejercicio2()
+
+def ejercicio3():
+    import random
+    vocales = ["a", "e", "i", "o", "u"]
+    while frase != "agusfortnite2008":
+        frase = str(input("Ingrese una frase: "))
+        frase_final = ""
+        for letra in frase:
+            if letra in vocales:
+                letra = random.choice(vocales)
+            frase_final = frase_final + letra
+
+def ejercicio4():
+    try:
+        frase = str(input("Ingrese una frase: "))
+        palabras = frase.split()
+        for letra in palabras:
+            print(letra[::-1])
+    except ValueError:
+        print("Ingresar un texto, no numeros.")
+
+def ejercicio5():
+    nombres = []
+    while 0 == 0:
+        print("Base de datos")
+        print("Ingrese que desea hacer: ")
+        print("1) Agregar nombre.")
+        print("2) Revisar nombre.")
+        print("3) Cerrar Programa.")
+        menu = int(input("--> "))
+        if menu == 1:
+            try:
+                nombre = str(input("Ingrese que nombre desea agregar: "))
+                nombres.append(nombre)
+                print(f"\n \n \n")
+            except ValueError:
+                print("")
+                print(f"\n \nIngrese un nombre, no números. \n")
+        elif menu == 2:
+            try:
+                buscar = int(input("Ingrese el número de índice del nombre que desea ver: "))
+                print(f"\n \n{nombres[buscar]} \n")
+            except (ValueError, IndexError):
+                print(f"\n \nPor favor, no ingrese un número de índice mayor al que esta disponible y no ingrese cosas que no sean números.. \n")
+        elif menu == 3:
+            print("Cerrando programa...")
+            break
+        else:
+            print(f"\n \nEsa opción no se encuentra disponible, intente otra vez. \n")
+
+while 0 == 0:
+    print("Este es el menú")
+    ejercicio = int(input("Ingrese el ejercicio desea corregir (Ingrese 0 para terminar): "))
+    if ejercicio == 1:
+        ejercicio1()
+    elif ejercicio == 2:
+        ejercicio2()
+    elif ejercicio == 3:
+        ejercicio3()
+    elif ejercicio == 4:
+        ejercicio4()
+    elif ejercicio == 5:
+        ejercicio5()
+    elif ejercicio == 0:
+        print("Finalizando programa...")
+
+        break
