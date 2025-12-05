53141decd425574de03600f56509e1ce6b17daff
MauroArmendia <mauro.armendiaet32@gmail.com>
2025-12-05 12:32:07 -0300

    Add files via upload
    Signed-off-by: MauroArmendia <mauro.armendiaet32@gmail.com>

---
+++ ejercicio2.14.py
@@ -0,0 +1,43 @@
+
+#Ejercicio 1
+informacion_personal = {
+    "nombre": "Mauro",
+    "edad": 16,
+    "ciudad": "Ciudad Incorrecta",
+    "profesion": "Profesión Incorrecta"
+}
+
+
+#Ejercicio 2
+informacion_personal["ciudad"] = "CABA"
+informacion_personal["profesion"] = "Estudiante"
+informacion_personal["telefono"] = 1158903918
+informacion_personal["email"] = "mauro.armendia.et32@gmail.com"
+
+
+#Ejercicio 3
+calificaciones = {
+    "matematica": 6,
+    "lengua": 8,
+    "ciencia": 8
+}
+print(calificaciones.get("matematica"))
+
+
+#Ejercicio 4
+promedio = calificaciones.get("matematica") + calificaciones.get("lengua") + calificaciones.get("ciencias")
+promedio = promedio / 3
+print(promedio)
+
+
+#Ejercicio 5
+paises = {
+    "Argentina": "Buenos Aires",
+    "Brasil": "Brasilia",
+    "Chile": "Santiago",
+    "Paraguay": "Asunción",
+    "Uruguay": "Montevideo"
+}
+
+pais_ingresado = input("Ingresá un país: ")
+print("La capital es:", paises.get(pais_ingresado, "País no encontrado"))
